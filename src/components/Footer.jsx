07fbbf765a8490783a1b60688071931491198b2b
DIGVIJAY <singhdigvijay703@gmail.com>
2023-04-06 00:06:58 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,48 +12,85 @@ const Footer = () => {
         sx={{
           background: "#0f212e",
           bottom: "0",
-          height: { xs: "auto", md: "40vh" },
-          padding: "2rem" 
+          height: { xs: "auto", md: "30vh" },
+          padding: "2rem",
+          color: "#fff",
+          textAlign: { xs: "center", md: "left" },
         }}
       >
-        <Grid container>
+        <Grid container spacing={1}>
           <Grid item sm={12} md={3}>
             <Logo />
-            <Box>
-              <InstagramIcon color="#fff" />
-              <FacebookIcon color="#fff" />
-              <TwitterIcon color="#fff" />
-              <LinkedInIcon color="#fff" />
+            <Box sx={{ color: "#fff", mt: 2 }}>
+              <InstagramIcon sx={{ fontSize: "2rem", mr: 1 }} />
+              <FacebookIcon sx={{ fontSize: "2rem", mr: 1 }} />
+              <TwitterIcon sx={{ fontSize: "2rem", mr: 1 }} />
+              <LinkedInIcon sx={{ fontSize: "2rem" }} />
             </Box>
           </Grid>
           <Grid item sm={12} md={3}>
-            <Typography variant="h5" component="h1">
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{
+                fontSize: { xs: "1rem", md: "2rem" },
+                fontWeight: "bold",
+              }}
+            >
               Company & Team
             </Typography>
             <ul>
-              <li>About</li>
-              <li>News & Blog</li>
-              <li>Press about us</li>
-              <li>Our partners</li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>About</li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>
+                News & Blog
+              </li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>
+                Press about us
+              </li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>
+                Our partners
+              </li>
             </ul>
           </Grid>
           <Grid item sm={12} md={3}>
-            <Typography variant="h5" component="h1">
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{
+                fontSize: { xs: "1rem", md: "2rem" },
+                fontWeight: "bold",
+              }}
+            >
               Help & Support
             </Typography>
             <ul>
-              <li>Support</li>
-              <li>FAQ</li>
-              <li>Supported Exchange</li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>Support</li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>FAQ</li>
+              <li
+                style={{ fontSize: "1rem", fontWeight: "500" }}
+              >
+                Supported Exchange
+              </li>
             </ul>
           </Grid>
           <Grid item sm={12} md={3}>
-            <Typography variant="h5" component="h1">
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{
+                fontSize: { xs: "1rem", md: "2rem" },
+                fontWeight: "bold",
+              }}
+            >
               Tool
             </Typography>
             <ul>
-              <li>Exchange Widgit</li>
-              <li>Paymet Button</li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>
+                Exchange Widget
+              </li>
+              <li style={{ fontSize: "1rem", fontWeight: "500" }}>
+                Payment Button
+              </li>
             </ul>
           </Grid>
         </Grid>
