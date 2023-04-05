c2f0fd8a554c688493bbd8e702aef9919468f73b
DIGVIJAY <singhdigvijay703@gmail.com>
2023-04-06 00:59:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import Logo from './components/common/Logo';
+import "./App.css";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Logo />
-    <Navbar />
-    <Signin />
-    <Signup />
-    <Footer />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Logo from './common/Logo';
+import SubmitButton from './common/Button';
 
-const Navbar = () => {
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Pricing', 'Sign In'];
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', bgcolor: '#0f212e' }}>
+      <Logo />
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center',
+              color: '#fff' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        <SubmitButton>Sign Up</SubmitButton>
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ display: 'flex', bgcolor: '#0f212e' }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ bgcolor: '#0f212e' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Logo />
+          </Box>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+            <SubmitButton sx={{ color: '#fff' }}>Sign Up</SubmitButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+              bgcolor: '#0f212e',
+              flexDirection: 'column',
+              justifyContent: "flex-start",
+              alignItems: "center",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Toolbar />
+    </Box>
+  );
 }
 
-export default Navbar
+Navbar.propTypes = {
+  window: PropTypes.func,
+};
+
+export default Navbar;
