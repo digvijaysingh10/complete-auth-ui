ec54f0c25f33bb09d9e791c490d975d225754c30
DIGVIJAY <singhdigvijay703@gmail.com>
2023-04-06 02:14:31 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,22 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Logo from './common/Logo';
-import SubmitButton from './common/Button';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Logo from "./common/Logo";
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Pricing', 'Sign In'];
+const navItems = ["Home", "About", "Pricing", "Sign In"];
 
 function Navbar(props) {
   const { window } = props;
@@ -28,49 +27,74 @@ function Navbar(props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', bgcolor: '#0f212e' }}>
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center", bgcolor: "#0f212e" }}
+    >
       <Logo />
       <Divider />
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center',
-              color: '#fff' }}>
+            <ListItemButton sx={{ textAlign: "center", color: "#fff" }}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
         ))}
-        <SubmitButton>Sign Up</SubmitButton>
+        <Button
+          sx={{
+            color: "#fff",
+            height: "2.5rem",
+            width: "8rem",
+            borderRadius: "2rem",
+            border: "none",
+            background: "#da2d92",
+          }}
+        >
+          Sign Up
+        </Button>
       </List>
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex', bgcolor: '#0f212e' }}>
+    <Box sx={{ display: "flex", bgcolor: "#0f212e" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ bgcolor: '#0f212e' }}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <AppBar component="nav" sx={{ bgcolor: "#0f212e" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
             <IconButton
               color="inherit"
               aria-label="open drawer"
               edge="start"
               onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
+              sx={{ mr: 2, display: { sm: "none" } }}
             >
               <MenuIcon />
             </IconButton>
             <Logo />
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
+              <Button key={item} sx={{ color: "#fff" }}>
                 {item}
               </Button>
             ))}
-            <SubmitButton sx={{ color: '#fff' }}>Sign Up</SubmitButton>
+            <Button
+              sx={{
+                color: "#fff",
+                height: "2.5rem",
+                width: "8rem",
+                borderRadius: "2rem",
+                border: "none",
+                background: "#da2d92",
+              }}
+            >
+              Sign Up
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
@@ -84,12 +108,12 @@ function Navbar(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
               width: drawerWidth,
-              bgcolor: '#0f212e',
-              flexDirection: 'column',
+              bgcolor: "#0f212e",
+              flexDirection: "column",
               justifyContent: "flex-start",
               alignItems: "center",
             },
